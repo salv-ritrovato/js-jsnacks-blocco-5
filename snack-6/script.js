6ac13a8a532806780595b7631eb270c93877364b
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-20 14:40:52 +0100

    task 6 and 7 completed

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,21 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+
+// Ho utilizzato il metodo filter per filtrare le zucchine per la loro lunghezza ma ciò mi ha costretto ad utilizzare due volte il metodo filter
+const longZucchini = zucchine.filter(long => {
+  if (long.length > 15) {
+    return true
+  }
+})
+console.log(longZucchini);
+
+const shortZucchini = zucchine.filter(short => {
+  if (short.length < 15) {
+    return true;
+  }
+})
+console.log(shortZucchini);
+
+// Avrei potuto usare forEach inizializzando due variabili con array vuoto ma ho preferito il filter
