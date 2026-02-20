ef043187f1532cf476c8a244ad2e5fff3e829dd0
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-20 14:22:10 +0100

    task 5 completed

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,15 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// Utilizzo il metodo filter per filtrare fra i numeri dell'array in cerca di quelli pari
+const evenNumbers = nums.filter(even => {
+    // Creo una condizionale if per cercare i numeri pari utilizzando l'operatore modulo
+    if (even % 2 === 0) {
+        return true;
+    }
+});
+
+console.log(evenNumbers);
+
+
